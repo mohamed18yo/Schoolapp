828b0ffe8cc0095cd35e248e02b0cc683b7f7187
mohamed yosef <mohamed18yosef@gmail.com>
2020-06-13 15:57:46 +0300

    first ap

--- routes/index.js
+++ routes/index.js
@@ -13,4 +13,12 @@ router.get('/login', function (req, res, next) {
   res.render('login');
 });
 
+router.post('/login', function (req, res, next){
+  if(req.body.password == "10"){
+res.redirect('/student')
+}else{
+  res.send("login error")
+}
+})
+
 module.exports = router;
